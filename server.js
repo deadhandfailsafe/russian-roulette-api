d9b7701a6d048ea55312f51e37f3dc105f2be6c3
kgideon <kgideon.dhfs@gmail.com>
2019-04-18 15:07:32 -0400

    Reverting to var base for now

--- server.js
+++ server.js
@@ -8,36 +8,32 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true
+//   }
+
+const database = {
+  amount: 0
+};
+
+app.get('/', (req, res) => {
+  res.json(database.amount);
+  // db.select('amount')
+  //   .from('deaths')
+  //   .then(data => {
+  //     res.send(data);
+  //   })
+  //   .catch(err => res.status(400).json('Unable to get amount.'));
 });
 
-app.get('/', (req, res, db) => {
-  db.select('*')
-    .from('deaths')
-    .then(data => {
-      console.log(data);
-    });
+app.put('/lose', (req, res) => {
+  // db('deaths')
+  //   .increment('amount', 1)
+  //   .catch(err => res.status(400).json('Unable to get amount.'));
 });
-
-// app.get('/', (req, res, db) => {
-//   db.select('amount')
-//     .from('deaths')
-//     .then(data => {
-//       res.send(data);
-//     })
-//     .catch(err => res.status(400).json('Unable to get amount.'));
-// });
-
-// app.put('/lose', (req, res, db) => {
-//   db('deaths')
-//     .increment('amount', 1)
-//     .catch(err => res.status(400).json('Unable to get amount.'));
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('Running on port ${process.env.PORT}');
