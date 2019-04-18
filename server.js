d1be9437ce358c015c134a293f9ae72b688428ba
kgideon <kgideon.dhfs@gmail.com>
2019-04-18 04:41:39 -0400

    init

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+app.use(bodyParser.json());
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  }
+});
+
+app.use(cors());
+
+const database = {
+  playerDeaths: 0
+};
+
+app.get('/', (req, res, db) => {
+  db('deaths')
+    .returning('amount')
+    .then(amount => {
+      res.send(amount);
+    })
+    .catch(err => res.status(400).json('Unable to get amount.'));
+});
+
+app.put('/lose', (req, res) => {
+  db('deaths')
+    .increment('amount', 1)
+    .returning('amount')
+    .then(amount => {
+      res.send(amount);
+    })
+    .catch(err => res.status(400).json('Unable to get amount.'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Running on port ${process.env.PORT}');
+});
